13b93841b7d9ed553ef50c89181de8bb82d3a571
Joshua Koo <zz85nus@gmail.com>
2015-12-27 04:09:31 +0800

    Import Project Potatoe
    - prototyping with cssgram from http://jabtunes.com/labs/potatoe/
    - start integrating electron version 0.36.2

---
+++ ImageOpener.js
@@ -0,0 +1,189 @@
+
+// Util Image Loader via HTML browser / drag and drop
+// Load 3. Integration as JS file for demo
+
+function ImageOpener( processImage, target ) {
+
+	var debug = true;
+	// Check for the various File API support.
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
+		// Great success! All the File APIs are supported.
+	} else {
+		alert('The File APIs are not fully supported in this browser.');
+	}
+
+	target = target || document.body;
+
+	// Input Field
+	var input = document.createElement('input');
+	input.type = 'file';
+	input.addEventListener('change', handleFileSelect, false);
+	this.input = input;
+
+	// target.appendChild(input);
+
+	target.onpaste = handlepaste;
+
+	/***************************************
+		Event handlers for file load
+	****************************************/
+	function handleFileSelect(evt) {
+		var files = evt.target.files; // FileList object
+
+		// files is a FileList of File objects. List some properties.
+		var output = [];
+
+		if (debug) {
+			for (var i = 0, f; f = files[i]; i++) {
+				console.log( 'detected', f.name, f.type,
+					f.size, ' bytes, last modified: ',
+					f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a'
+				);
+			}
+		}
+
+		// if (files[0]) {
+		// 	openFile(files[0]);
+		// }
+	}
+
+
+	var ii, iii;
+	function handlepaste (e) {
+		if (e && e.clipboardData && e.clipboardData.getData) {// Webkit - get data from clipboard, put into editdiv, cleanup, then cancel event
+			// ii = e.clipboardData.getData('jpg/image');
+			var items = e.clipboardData.items;
+
+			var i = 0;
+			if (items[i].kind == 'file' &&
+			items[i].type.indexOf('image/') !== -1) {
+				var blob = items[i].getAsFile();
+				window.URL = window.URL || window.webkitURL;
+				var blobUrl = window.URL.createObjectURL(blob);
+
+				var img = document.createElement('img');
+				img.src = blobUrl;
+				img.onload = function(e) {
+					processImage(img);
+				};
+			}
+
+			// console.log(e, e.clipboardData.getData('text/plain'));
+			if (e.preventDefault) {
+					e.stopPropagation();
+					e.preventDefault();
+			}
+			return false;
+		}
+	}
+	/***************************************
+		Event handlers for drag and drop
+	****************************************/
+	target.addEventListener('dragover', handleDragOver, false);
+	target.addEventListener('dragenter', stopDefault, false);
+	target.addEventListener('dragexit', stopDefault, false);
+
+	target.addEventListener('drop', dropBehavior, false);
+
+	/***************************************
+		Event Callbacks for drag and drop
+	****************************************/
+
+
+	function handleDragOver(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+	}
+
+
+	function stopDefault(e) {
+		e.preventDefault();
+		return false;
+	}
+
+	function dropBehavior(e) {
+		console.log('dropped');
+		e.stopPropagation();
+		e.preventDefault();
+		console.log(e);
+		var files = e.dataTransfer.files;
+		if (files.length) {			
+			for (var i = 0; i < files.length; i++) {
+				openFile(files[i]);
+			}
+		} else {
+			// debugger;
+			// TODO support copypaste/clipboard drag in
+		}
+	}
+
+	function openFile(file) {
+		if (!file.type.match('image.*')) {
+			if (debug) console.log('image fail');
+			return;
+		}
+
+		var reader = new FileReader();
+		reader.onloadend = loadImage;
+
+		reader.onerror = errorHandler;
+
+		reader.onprogress = updateProgress;
+		reader.onabort = function(e) {
+			console.log('File read cancelled');
+		};
+
+		reader.onloadstart = function(e) {
+			console.log('onloadstart');
+		};
+
+		reader.onload = function(e) {
+			console.log('onload');
+		};
+
+		// reader.readAsBinaryString(file);
+		reader.readAsDataURL(file);
+	}
+
+	function loadImage(e) {
+
+		var target = e.target;
+		console.log('loadImage', e); // e, target, reader, e.target.result
+		var img = document.createElement("img");
+
+		img.src = e.target.result;
+		img.onload = function(e) {
+			processImage(img);
+		};
+	}
+
+
+	function updateProgress(e) {
+		// evt is an ProgressEvent.
+		if (e.lengthComputable) {
+			var percentLoaded = Math.round((e.loaded / e.total) * 100);
+
+			if (percentLoaded < 100) {
+				console.log(percentLoaded + '%');
+			}
+		}
+	}
+
+
+	function errorHandler(e) {
+		switch (e.target.error.code) {
+			case e.target.error.NOT_FOUND_ERR:
+				alert('File Not Found!');
+				break;
+			case e.target.error.NOT_READABLE_ERR:
+				alert('File is not readable');
+				break;
+			case e.target.error.ABORT_ERR:
+				break; // noop
+			default:
+				alert('An error occurred reading this file.' + e);
+		}
+	}
+
+}
---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict';
+const electron = require('electron');
+const app = electron.app;
+
+// prevent window being garbage collected
+let mainWindow;
+
+function onClosed() {
+	// dereference the window
+	// for multiple windows store them in an array
+	mainWindow = null;
+}
+
+function createMainWindow() {
+	const win = new electron.BrowserWindow({
+		width: 600,
+		height: 400
+	});
+
+	win.loadURL(`file://${__dirname}/index.html`);
+	win.on('closed', onClosed);
+
+	return win;
+}
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate-with-no-open-windows', () => {
+	if (!mainWindow) {
+		mainWindow = createMainWindow();
+	}
+});
+
+app.on('ready', () => {
+	mainWindow = createMainWindow();
+});
