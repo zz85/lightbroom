3dc48d82e19931c0c4ffe861624baa69d146a8c1
Joshua Koo <zz85nus@gmail.com>
2015-12-27 23:55:18 +0800

    Add more SVG filter effects

--- svg-image.js
+++ svg-image.js
@@ -35,14 +35,100 @@ function inlineSVGImg(data, width, height) {
 					<feFuncA type="identity"/>
 				</feComponentTransfer>
 			</filter>
-			
+
 			<filter id="css_brightness">
 				<feComponentTransfer>
-				<feFuncR type="linear" slope="0.5"/>
-				<feFuncG type="linear" slope="0.5"/>
-				<feFuncB type="linear" slope="0.5"/>
+					<feFuncR type="linear" slope="0.5"/>
+					<feFuncG type="linear" slope="0.5"/>
+					<feFuncB type="linear" slope="0.5"/>
 				</feComponentTransfer>
 			</filter>
+			<filter id="brightness_threshold">
+				<feComponentTransfer>
+					<feFuncR type="linear" slope="1.5" intercept="-0.3"/>
+					<feFuncG type="linear" slope="1.5" intercept="-0.3"/>
+					<feFuncB type="linear" slope="1.5" intercept="-0.3"/>
+				</feComponentTransfer>
+			</filter>
+
+			<filter id="css_opacity">
+				<feComponentTransfer>
+					<feFuncA type="table" tableValues="0 0.5"/>
+				</feComponentTransfer>
+			</filter>
+
+			<filter id="css_invert">
+				<feComponentTransfer>
+					<feFuncR type="table" tableValues="1 0"/>
+					<feFuncG type="table" tableValues="1 0"/>
+					<feFuncB type="table" tableValues="1 0"/>
+				</feComponentTransfer>
+			</filter>
+
+			<filter id="gamma_correct">
+				<feComponentTransfer>
+					<feFuncG type="gamma" amplitude="1" exponent="0.5"/>
+				</feComponentTransfer>
+			</filter>
+
+			<filter id="gamma_correct2">
+				<feComponentTransfer>
+				<feFuncG type="gamma" amplitude="1" exponent="0.5"
+						offset="-0.1"/>
+				</feComponentTransfer>
+			</filter>
+
+			<filter id="css_grayscale">
+ 				<feColorMatrix type="saturate" values="0"/>
+			</filter>
+
+			<filter id="css_saturate">
+				<feColorMatrix type="saturate" values="10"/>
+			</filter>
+
+			<filter id="css_hue_rotate">
+				<feColorMatrix type="hueRotate" values="180"/>
+			</filter>
+
+			<filter id="luminance_mask">
+				<feColorMatrix type="luminanceToAlpha"/>
+			</filter>
+
+			<filter id="css_sepia">
+				<feColorMatrix
+				type="matrix"
+				values=".343 .669 .119 0 0
+						.249 .626 .130 0 0
+						.172 .334 .111 0 0
+						.000 .000 .000 1 0 "/>
+			</filter>
+
+			<filter id="dusk">
+				<feColorMatrix
+					type="matrix"
+					values="1.0 0   0   0   0
+							0   0.2 0   0   0
+							0   0   0.2 0   0
+							0   0   0   1.0 0 "/>
+			</filter>
+
+			<filter id="extremeEffect">
+				<feConvolveMatrix order="5" kernelMatrix="1 1 1 1 1 1 -2 -2 -2 1 1  -2 .01 -2 1 1 -2 -2 -2 1 1 1 1 1 1"/>
+ 				<feColorMatrix type="luminanceToAlpha"/>
+			</filter>
+
+			<filter id="css_drop_shadow">
+				<feGaussianBlur stdDeviation="2"  in="SourceAlpha" />
+				<feOffset dx="4" dy="6"           result="offsetblur"/>
+				<feFlood flood-color="#777"/>
+				<feComposite operator="in"        in2="offsetblur"/>
+				<feMerge>
+					<feMergeNode/>
+					<feMergeNode                    in="SourceGraphic"/>
+				</feMerge>
+			</filter>
+
+
 		</defs>
 		<style>
 		/* <![CDATA[ */
